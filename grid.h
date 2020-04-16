37f364846da3820222c06c3e7a027af7e0e7eaff
Nicholas Schweitzer <f3bl@gmx.com>
2020-04-16 10:26:27 +0200

    Next_gen function

--- grid.h
+++ grid.h
@@ -12,7 +12,45 @@ char grid_get(Grid *grid, unsigned x, unsigned y) {
 }
 
 void grid_set(Grid *grid, unsigned x, unsigned y, char val) {
-    grid->state[y * grid->width + x] = val;
+    // will not set the padding
+    if (x != 0 && x != grid->width - 1 && y != 0 && y != grid->height - 1)
+        grid->state[y * grid->width + x] = val;
+}
+
+char count_neighbors(Grid *grid, unsigned x, unsigned y) {
+    return grid_get(grid, x - 1, y - 1)
+        + grid_get(grid, x - 1, y    )
+        + grid_get(grid, x - 1, y + 1)
+        + grid_get(grid, x    , y - 1)
+        + grid_get(grid, x    , y + 1)
+        + grid_get(grid, x + 1, y - 1)
+        + grid_get(grid, x + 1, y    )
+        + grid_get(grid, x + 1, y + 1);
+}
+
+void grid_gen(Grid *grid) {
+    for (unsigned y = 1; y < grid->height - 1; y++) {
+        for (unsigned x = 1; x < grid->width - 1; x++) {
+            char neighbors = count_neighbors(grid, x, y);
+            if (grid_get(grid, x, y) == 1) {
+                // is alive
+                if (neighbors == 2 || neighbors == 3)
+                    grid->next_state[y * grid->width + x] = 1;
+                else
+                    grid->next_state[y * grid->width + x] = 0;
+            } else {
+                // is dead
+                if (neighbors == 3)
+                    grid->next_state[y * grid->width + x] = 1;
+                else
+                    grid->next_state[y * grid->width + x] = 0;
+            }
+        }
+    }
+
+    char *tmp = grid->state;
+    grid->state = grid->next_state;
+    grid->next_state = tmp;
 }
 
 Grid grid_new(unsigned width, unsigned height) {
---
+++ tests/next_gen/test.c
@@ -0,0 +1,25 @@
+#include "../../grid.h"
+#include "../../view.h"
+#include "../../save.h"
+
+int main() {
+    FILE *fp = fopen("saved.gol", "r");
+
+    if (fp == NULL) {
+        printf("Could not load saved.gol\n");
+        exit(1);
+    }
+
+    Grid grid;
+    if (load_grid(&grid, fp) != 0) {
+        printf("Could not load grid\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 50; i++) {
+        printf("Generation %d:\n", i);
+        print_grid(&grid);
+        printf("\n");
+        grid_gen(&grid);
+    }
+}
